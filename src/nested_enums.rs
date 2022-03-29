703089e99089ac806def81d0346f0ac27ce8c32f
scepea <scepea@gmail.com>
2022-03-29 14:06:12 +0100

    Added use statements to reduce the verbosity of adding nested components.

--- src/nested_enums.rs
+++ src/nested_enums.rs
@@ -1,11 +1,8 @@
 use std::collections::HashSet;
 use std::fmt::Debug;
-
-#[derive(PartialEq, Eq, Hash, Debug)]
-enum PhysicsComponents {
-    RigidBody,
-    MeshCollider,
-}
+use Components::PhysicsComponent;
+use PhysicsComponents::RigidBody;
+use PhysicsComponents::MeshCollider;
 
 #[derive(PartialEq, Eq, Hash, Debug)]
 enum SpriteComponents {
@@ -14,17 +11,23 @@ enum SpriteComponents {
 }
 
 #[derive(PartialEq, Eq, Hash, Debug)]
+enum PhysicsComponents {
+    RigidBody,
+    MeshCollider,
+}
+
+#[derive(PartialEq, Eq, Hash, Debug)]
 enum Components {
-    PhysicsComponent(PhysicsComponents),
     SpriteComponent(SpriteComponents),
+    PhysicsComponent(PhysicsComponents),
 }
 
 pub fn example() {
     let mut components:HashSet<Components> = HashSet::new();
-    components.insert(Components::PhysicsComponent(PhysicsComponents::RigidBody));
-    components.insert(Components::PhysicsComponent(PhysicsComponents::MeshCollider));
     components.insert(Components::SpriteComponent(SpriteComponents::Sprite));
     components.insert(Components::SpriteComponent(SpriteComponents::Atlas));
+    components.insert(PhysicsComponent(RigidBody));
+    components.insert(PhysicsComponent(MeshCollider));
     for x in components {
         println!("{:?}", x);
     }
