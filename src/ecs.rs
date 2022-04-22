1292085b8b6e89e493903e9cf3d861f50e30de13
scepea <scepea@gmail.com>
2022-04-22 09:29:01 +0100

    Added query functionality to ECS, bringing it to MVP stage.

--- src/ecs.rs
+++ src/ecs.rs
@@ -50,7 +50,59 @@ impl Entity {
         }
     }
 
+    pub fn has_component<T: Component>(&self) -> bool{
+        match self.components.get(&TypeId::of::<T>()) {
+            Some(x) => true,
+            None => false
+        }
+    }
+
 }
+
+struct World {
+    entities: HashSet<Entity>
+}
+
+impl World {
+    pub fn new() -> World{
+        World{entities: HashSet::new()}
+    }
+
+    pub fn query(&self, query: Query) -> HashSet<&Entity> {
+        let mut result: HashSet<&Entity> = HashSet::new();
+        for entity in &self.entities {
+            if query.include.iter().all(|k| entity.components.contains_key(k)) {
+                result.insert(entity);
+            }
+        }
+        result
+    }
+
+    pub fn add_entity(&mut self, entity: Entity) {
+        self.entities.insert(entity);
+    }
+
+    pub fn remove_entity(&mut self, entity: &Entity) {
+        self.entities.remove(entity);
+    }
+}
+
+struct Query {
+    include: HashSet<TypeId>
+}
+
+impl Query {
+    pub fn new() -> Query {
+        Query { include: HashSet::new() }
+    }
+
+    pub fn with<T: Component>(mut self) -> Self{
+        self.include.insert(TypeId::of::<T>());
+        self
+    }
+}
+
+
 
 pub fn example() {
     let mut entities: HashSet<Entity> = HashSet::new();
@@ -79,4 +131,87 @@ pub fn example() {
         Some(_) => panic!("The entity shouldn't have a name component anymore!"),
         None => println!("Now the entity is now nameless, as expected.")
     }
+}
+
+#[cfg(test)]
+mod test {
+    use super::{World, Component, Entity, Query};
+    use std::any::Any;
+
+    #[derive(Component)]
+    struct StringComponent{
+        name: String
+    }
+
+    #[derive(Component)]
+    struct IntComponent{
+        number: u32
+    }
+    
+    #[test]
+    fn test_single_result(){
+        // Given
+        let mut world = World::new();
+        let mut e1 = Entity::new();
+        let mut e2 = Entity::new();
+
+        let sc1 = StringComponent{name: String::from("First Entity")};
+        let sc2 = StringComponent{name: String::from("Second Entity")};
+
+        let ic = IntComponent{number: 1138};
+
+        let query = Query::new().with::<StringComponent>().with::<IntComponent>();
+        // When
+        e1.add_component(sc1);
+        e2.add_component(sc2);
+        e2.add_component(ic);
+
+        world.add_entity(e1);
+        world.add_entity(e2);
+
+        let actual = world.query(query);
+        
+        // Then
+        assert_eq!(1, actual.len());
+        for result in actual {
+            assert_eq!(String::from("Second Entity"), result.get_component::<StringComponent>().expect("").name);
+            assert_eq!(1138, result.get_component::<IntComponent>().expect("").number);
+        }
+    }
+
+    #[test]
+    fn test_multiple_result(){
+        // Given
+        let mut world = World::new();
+        let mut e1 = Entity::new();
+        let mut e2 = Entity::new();
+
+        let sc1 = StringComponent{name: String::from("First Entity")};
+        let sc2 = StringComponent{name: String::from("Second Entity")};
+
+        let ic = IntComponent{number: 1138};
+
+        let query = Query::new().with::<StringComponent>();
+        // When
+        e1.add_component(sc1);
+        e2.add_component(sc2);
+        e2.add_component(ic);
+
+        world.add_entity(e1);
+        world.add_entity(e2);
+
+        let actual = world.query(query);
+        
+        // Then
+        assert_eq!(2, actual.len());
+        for result in actual {
+            if result.has_component::<IntComponent>() {
+                assert_eq!(String::from("Second Entity"), result.get_component::<StringComponent>().expect("").name);
+                assert_eq!(1138, result.get_component::<IntComponent>().expect("").number);
+            } else {
+                assert_eq!(String::from("First Entity"), result.get_component::<StringComponent>().expect("").name);
+            }
+        }
+    }
+
 }
